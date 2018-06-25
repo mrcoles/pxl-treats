682983303ed8f46714a98b3476660168b6cebbb5
Peter Coles <peter@mrcoles.com>
2018-06-25 17:53:16 -0400

    got dot rendering

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,19 @@ extern crate pxl;
 use pxl::*;
 
 const INCR: f32 = 0.1;
+const WIDTH: i32 = 512;
+const HEIGHT: i32 = 512;
+
+#[derive(Copy, Clone)]
+struct Dot {
+    x: i32,
+    y: i32
+}
 
 struct Daisy {
     red: f32,
     green: f32,
+    dot: Dot
 }
 
 impl Program for Daisy {
@@ -14,6 +23,7 @@ impl Program for Daisy {
         Daisy {
             red: 0.5,
             green: 0.5,
+            dot: Dot { x: WIDTH / 2, y: HEIGHT / 2 }
         }
     }
 
@@ -23,13 +33,15 @@ impl Program for Daisy {
 
     fn render(&mut self, pixels: &mut [Pixel]) {
         for (i, pixel) in pixels.iter_mut().enumerate() {
-            *pixel = Pixel{
+            *pixel = Pixel {
                 red: self.red,
                 green: self.green,
                 blue: (i as f32 * 0.01 + 1.0) % 1.0,
                 alpha: 1.0
             };
         }
+
+        self.draw_dot(pixels);
     }
 
     fn tick(&mut self, events: &[Event]) {
@@ -39,8 +51,8 @@ impl Program for Daisy {
         for event in events {
             if let Event::Button { state: ButtonState::Pressed, button } = event {
                 match button {
-                    Button::Down => red_dir = -1,
                     Button::Up => red_dir = 1,
+                    Button::Down => red_dir = -1,
                     Button::Left => green_dir = -1,
                     Button::Right => green_dir = 1,
                     _ => {},
@@ -53,10 +65,29 @@ impl Program for Daisy {
     }
 }
 
+impl Daisy {
+    fn draw_dot(&self, pixels: &mut [Pixel]) {
+        let dot = self.dot;
+
+        for dx in -5..5 {
+            for dy in -5..5 {
+                let i = index_of(dot.x + dx, dot.y + dy);
+                pixels[i] = Pixel {
+                    red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0
+                }
+            }
+        }
+    }
+}
+
 // ## helpers
 
 fn clampf(val: f32, min: f32, max: f32) -> f32 {
     val.min(max).max(min)
+}
+
+fn index_of(x: i32, y: i32) -> usize {
+    (y * WIDTH + x) as usize
 }
 
 // ## main
