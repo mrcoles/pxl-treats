f0bb5612e9a44a1525261afa7caf9a42b37eb54b
Peter Coles <peter@mrcoles.com>
2018-06-26 13:24:47 -0400

    create renderable trait

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,35 @@ extern crate pxl;
 
 use pxl::*;
 
+// ## Constants
+
 const WIDTH: i32 = 512;
 const HEIGHT: i32 = 512;
+const DOT_WIDTH: i32 = 11;
+const DOT_HEIGHT: i32 = 11;
 
 const INCR_DOT: i32 = WIDTH / 50;
 const INCR_COLOR: f32 = 0.05;
+
+// const BLACK: Pixel = Pixel { red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0 };
+const WHITE: Pixel = Pixel { red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0 };
+
+// ## Traits
+
+trait Renderable {
+    /// centered x-position
+    fn get_x(&self) -> i32;
+    /// centered y-position
+    fn get_y(&self) -> i32;
+    /// width of element
+    fn get_width(&self) -> i32;
+    /// height of element
+    fn get_height(&self) -> i32;
+}
+
+// ## Structs
+
+// Dot
 
 #[derive(Copy, Clone)]
 struct Dot {
@@ -14,10 +38,64 @@ struct Dot {
     y: i32
 }
 
+impl Renderable for Dot {
+    fn get_x(&self) -> i32 {
+        self.x
+    }
+    fn get_y(&self) -> i32 {
+        self.y
+    }
+    fn get_width(&self) -> i32 {
+        DOT_WIDTH
+    }
+    fn get_height(&self) -> i32 {
+        DOT_HEIGHT
+    }
+}
+
+// Target
+
+#[derive(Copy, Clone)]
+struct Target {
+    x: i32,
+    y: i32,
+    is_alive: bool,
+    death_steps: i8
+}
+
+impl Target {
+    fn new(_x: i32, _y: i32) -> Target {
+        Target {
+            x: _x,
+            y: _x,
+            is_alive: true,
+            death_steps: 10
+        }
+    }
+}
+
+impl Renderable for Target {
+    fn get_x(&self) -> i32 {
+        self.x
+    }
+    fn get_y(&self) -> i32 {
+        self.y
+    }
+    fn get_width(&self) -> i32 {
+        DOT_WIDTH
+    }
+    fn get_height(&self) -> i32 {
+        DOT_HEIGHT
+    }
+}
+
+// Daisy
+
 struct Daisy {
     red: f32,
     green: f32,
-    dot: Dot
+    dot: Dot,
+    target: Target
 }
 
 impl Program for Daisy {
@@ -25,7 +103,8 @@ impl Program for Daisy {
         Daisy {
             red: 0.5,
             green: 0.5,
-            dot: Dot { x: WIDTH / 2, y: HEIGHT / 2 }
+            dot: Dot { x: WIDTH / 2, y: HEIGHT / 2 },
+            target: Target::new(0, 0)
         }
     }
 
@@ -43,7 +122,16 @@ impl Program for Daisy {
             };
         }
 
-        self.draw_dot(pixels);
+        let color = Pixel {
+            red: 1.0 - self.red,
+            green: 1.0 - self.green,
+            blue: 0.5, alpha: 1.0,
+        };
+        self.draw(pixels, color, &self.dot);
+
+        self.target.x = 20;
+        self.target.y = 20;
+        self.draw(pixels, WHITE, &self.target);
     }
 
     fn tick(&mut self, events: &[Event]) {
@@ -85,17 +173,29 @@ impl Program for Daisy {
 }
 
 impl Daisy {
-    fn draw_dot(&self, pixels: &mut [Pixel]) {
-        let dot = self.dot;
-        let color = Pixel { red: 1.0 - self.red, green: 1.0 - self.green, blue: 0.5, alpha: 1.0 };
+    fn draw(&self, pixels: &mut [Pixel], color: Pixel, r: &Renderable) {
+        let width = r.get_width();
+        let height = r.get_height();
+        let x = r.get_x();
+        let y = r.get_y();
 
-        for dx in -5..=5 {
-            for dy in -5..=5 {
-                if let Some(i) = index_of(dot.x + dx, dot.y + dy) {
+        let x_start = x - width / 2;
+        let x_end = x + width / 2;
+
+        let y_start = y - height / 2;
+        let y_end = y + height / 2;
+
+        for tx in x_start..=x_end {
+            for ty in y_start..=y_end {
+                if let Some(i) = index_of(tx, ty) {
                     pixels[i] = color;
                 }
             }
         }
+    }
+
+    fn is_collision(&self) -> bool {
+        
     }
 }
 
